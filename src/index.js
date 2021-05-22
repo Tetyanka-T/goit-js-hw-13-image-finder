81069d59278a4f0e8b730b21a5f40a61ee2a692f
Tatyana Tupalo <tatyana080489t@gmail.com>
2021-05-22 17:58:02 +0300

    fetch

--- src/index.js
+++ src/index.js
@@ -1,2 +1,46 @@
-import './js/apiService';
-// import './sass/main.scss';
+import ApiService from './js/apiService';
+import photoCards from './templates/photo-card';
+import './sass/main.scss';
+
+
+const refs = {
+    searchForm: document.querySelector('#search-form'),
+    galleryList: document.querySelector('.gallery'),
+    showMoreBtn: document.querySelector('.show-more-button'),
+}
+
+const apiService = new ApiService ();
+
+refs.searchForm.addEventListener('submit', onSearch);
+refs.showMoreBtn.addEventListener('click', onLoadMore);
+
+
+
+function onSearch (e) {
+    e.preventDefault();
+
+clearContainer ()
+    apiService.query = e.currentTarget.elements.query.value;
+    apiService.resetPage();
+    apiService.fetchPhoto().then(addGallery);
+
+}
+
+function onLoadMore () {
+    apiService.fetchPhoto().then(addGallery);
+}
+
+function addGallery (hits) {
+    refs.galleryList.insertAdjacentHTML('beforeend', photoCards(hits));
+
+    window.scrollTo(0, 1000);
+
+    window.scrollTo({
+      top: 1000,
+      behavior: 'smooth',
+    });
+}
+
+function clearContainer () {
+    refs.galleryList.innerHTML = '';
+}
--- src/js/apiService.js
+++ src/js/apiService.js
@@ -1,6 +1,28 @@
-import axios from 'axios';
+export default class ApiService {
+  constructor() {
+    this.searhQuery = '';
+    this.page = 1;
+  }
 
-
-
-axios.defaults.baseURL = 'https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=что_искать&page=номер_страницы&per_page=12&key=твой_ключ';
-axios.defaults.headers.common.Authorization = '21724467-4ce0638ba06e77e7c079dfe7e';
+  fetchPhoto() {
+      console.log(this);
+    const url = `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${this.searhQuery}&page=${this.page}&per_page=12&key=21724467-4ce0638ba06e77e7c079dfe7e`;
+    // const autKey = '21724467-4ce0638ba06e77e7c079dfe7e';
+    return fetch(url)
+      .then(r => r.json())
+      .then(photo => {
+          this.page +=1;
+          
+          return photo.hits;
+      });
+  }
+  resetPage () {
+      this.page = 1;
+  }
+  get query() {
+    return this.searhQuery;
+  }
+  set query(newQuery) {
+      this.searhQuery = newQuery;
+  }
+}
